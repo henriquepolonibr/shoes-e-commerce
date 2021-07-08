bc6f2b92d2aa3f22552cc6c3639fa61e439d9acb
henriquepolonibr <hpoloni11@gmail.com>
2021-07-08 12:23:14 -0300

    Page title update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Routes from './routes';
 
 export default function App() {
+  useEffect(() => {
+    document.title = 'React - Trustly';
+  }, []);
   return (
     <div>
       <Routes />
